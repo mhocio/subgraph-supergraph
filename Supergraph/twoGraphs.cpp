81c38aa7cb9d6f2613bb7a1ae615bc78b8bb54a8
Mikolaj Hoscilo <mikolaj.hoscilo@intel.com>
2021-11-21 19:51:47 +0100

    Add reordering solution to look simillar to the second graph

--- Supergraph/twoGraphs.cpp
+++ Supergraph/twoGraphs.cpp
@@ -1,21 +1,21 @@
 #include "twoGraphs.h"
 
-struct graphComarison_t {
-	bool correctMappingFound; // if graph are same
-	int missingEdges;
-	int redundantEdges;
-	std::vector<std::vector<int>> bestGraph;
-};
+void printGraphComarison_t(graphComarison_t g) {
+	std::cout << "Number of missing edges: " << g.missingEdges;
+	std::cout << " Number of redundant edges: " << g.redundantEdges << "\n";
+}
 
 twoGraphs::twoGraphs()
 {
 	approximateComputed = false;
 	exactComputed = false;
 	readFromFfile = false;
+	editedSolutionAfterTwoAlgorithms = false;
 }
 
 twoGraphs::twoGraphs(std::string inputFile)
 {
+	twoGraphs();
 	GraphReader reader;
 	reader.readInput(inputFile.c_str());
 	// making sure the graph with most number of vertices is assigned first
@@ -30,8 +30,6 @@ twoGraphs::twoGraphs(std::string inputFile)
 	approximateMaximalSubgraphAlgorithm = ApproximateMaximalSubgraph(graph1, graph2);
 	exactAlgorithm = ExactSubgraph(graph1, graph2);
 
-	approximateComputed = false;
-	exactComputed = false;
 	readFromFfile = true;
 }
 
@@ -59,8 +57,50 @@ void twoGraphs::printGraph(std::vector<std::vector<int>> G)
 	std::cout << "\n";
 }
 
+void twoGraphs::editSolutionAfterTwoAlgorithms() {
+	if (exactComputed && approximateComputed) {
+		compareSupergraphs = compareGraphs(exactAlgorithm.minimalSupergraph, approximateMinimalSupergraphAlgorithm.minimalCommonSupergraph);
+		// change the bigger graph to be simillar to the smaller one
+		if (exactMinimalSupergraph.size() >= approximateMinimalSupergraph.size()) {
+			exactMinimalSupergraph = compareSupergraphs.bestGraph;
+		}
+		else {
+			approximateMinimalSupergraph = compareSupergraphs.bestGraph;
+		}
+
+		compareSubgraphs = compareGraphs(exactMaximalSubgraph, approximateMaximalSubgraph);
+		// change the bigger graph to be simillar to the smaller one
+		if (exactMaximalSubgraph.size() >= approximateMaximalSubgraph.size()) {
+			exactMaximalSubgraph = compareSubgraphs.bestGraph;
+		}
+		else {
+			approximateMaximalSubgraph = compareSubgraphs.bestGraph;
+		}
+
+		editedSolutionAfterTwoAlgorithms = true;
+	}
+}
+
 void twoGraphs::printSolution()
 {
+	if (exactComputed && approximateComputed && editedSolutionAfterTwoAlgorithms) {
+		if (compareSupergraphs.correctMappingFound) {
+			std::cout << "Supergraphs of two algorithms are same!\n";
+		}
+		else {
+			std::cout << "Supergraphs of two algorithms are NOT same... ";
+			printGraphComarison_t(compareSupergraphs);
+		}
+
+		if (compareSubgraphs.correctMappingFound) {
+			std::cout << "Common subgraphs of two algorithms are same!\n";
+		}
+		else {
+			std::cout << "Common subgraphs of two algorithms are NOT same... ";
+			printGraphComarison_t(compareSubgraphs);
+		}
+	}
+
 	if (approximateComputed) {
 		std::cout << "Approximate algorithm computed\n";
 		std::cout << "approximateMaximalSubgraph\n";
@@ -76,29 +116,13 @@ void twoGraphs::printSolution()
 		std::cout << "exactMaximalSubgraph\n";
 		printGraph(exactMaximalSubgraph);
 	}
-
-	if (exactComputed && approximateComputed) {
-		auto compareSupergraphs = compareGraphs(exactAlgorithm.minimalSupergraph, approximateMinimalSupergraphAlgorithm.minimalCommonSupergraph);
-		if (compareSupergraphs.correctMappingFound) {
-			std::cout << "Supergraphs of two algorithms are same!\n";
-		}
-		else {
-			std::cout << "Supergraphs of two algorithms are NOT same...\n";
-		}
-
-		auto compareSubgraphs = compareGraphs(exactAlgorithm.maximalCommonSubgraph, approximateMaximalSubgraphAlgorithm.maximalCommonSubgraph);
-		if (compareSubgraphs.correctMappingFound) {
-			std::cout << "Common subgraphs of two algorithms are same!\n";
-		}
-		else {
-			std::cout << "Common subgraphs of two algorithms are NOT same...\n";
-		}
-	}
 }
 
-// second graph must be bigger or equal to the second one
+// return changed bigger graph simillar to the smaller one
 struct graphComarison_t twoGraphs::compareGraphs(std::vector<std::vector<int>> G1, std::vector<std::vector<int>> G2) {
-	if (G1.size() > G2.size()) {
+	// first graph must be bigger or equal to the second one
+	// as we permutate the bigger graph
+	if (G1.size() < G2.size()) {
 		std::swap(G1, G2);
 	}
 
@@ -111,21 +135,27 @@ struct graphComarison_t twoGraphs::compareGraphs(std::vector<std::vector<int>> G
 	graphComarison_t ret;
 
 	for (auto permOfExactGraph : permutationsOfExactGraph) {
-		auto reorderedGraph = exactAlgorithm.generateReorderedGraph(permOfExactGraph, G1);
+		auto reorderedGraph = exactAlgorithm.generateReorderedGraph(permOfExactGraph, G1);  // bigger graph
 		bool correctMappingFound = true;
 		int missingEdges = 0;
 		int redundantEdges = 0;
 
 		for (int i = 0; i < G1.size(); i++) {
 			for (int j = 0; j < G1[i].size(); j++) {
-				if (reorderedGraph[i][j] != G2[i][j]) {
-					correctMappingFound = false;
-					/*if (reorderedGraph[i][j] == 0) {
-						missingEdges++;
+
+				if (i < G2.size() && j < G2.size()) { // G2 within the bigger graph (G1)
+					if (reorderedGraph[i][j] != G2[i][j]) {
+						correctMappingFound = false;
+						if (reorderedGraph[i][j] == 1) {
+							missingEdges++;
+						}
+						else {
+							redundantEdges++;
+						}
 					}
-					else {
-						redundantEdges++;
-					}*/
+				} else if (reorderedGraph[i][j] == 1){ // G1 biggger than G1
+					missingEdges++;
+					correctMappingFound = false;
 				}
 			}
 		}
@@ -133,7 +163,7 @@ struct graphComarison_t twoGraphs::compareGraphs(std::vector<std::vector<int>> G
 		if (correctMappingFound) {
 			finalCorrectMappingFound = true;
 		}
-		else if ((minMissingEdges + minRedundantEdges) > (missingEdges + redundantEdges)) {
+		else if (((long long int)minMissingEdges + (long long int)minRedundantEdges) > ((long long int)missingEdges + (long long int)redundantEdges)) {
 			minMissingEdges = missingEdges;
 			minRedundantEdges = redundantEdges;
 			bestGraph = reorderedGraph;
@@ -155,6 +185,10 @@ void twoGraphs::computeExactSolution()
 	exactMinimalSupergraph = exactAlgorithm.minimalSupergraph;
 
 	exactComputed = true;
+
+	if (!editedSolutionAfterTwoAlgorithms) {
+		editSolutionAfterTwoAlgorithms();
+	}
 }
 
 void twoGraphs::computeApproximateSolution()
@@ -168,4 +202,8 @@ void twoGraphs::computeApproximateSolution()
 	approximateMinimalSupergraph = approximateMinimalSupergraphAlgorithm.minimalCommonSupergraph;
 
 	approximateComputed = true;
+
+	if (!editedSolutionAfterTwoAlgorithms) {
+		editSolutionAfterTwoAlgorithms();
+	}
 }
--- Supergraph/twoGraphs.h
+++ Supergraph/twoGraphs.h
@@ -9,6 +9,13 @@
 #include "ApproximateMinimalSupergraph.h"
 #include "ExactMaximalSubgraph.h"
 
+struct graphComarison_t {
+	bool correctMappingFound; // if graph are same
+	int missingEdges;
+	int redundantEdges;
+	std::vector<std::vector<int>> bestGraph;
+};
+
 class twoGraphs
 {
 public:
@@ -38,8 +45,13 @@ public:
 	struct graphComarison_t compareGraphs(std::vector<std::vector<int>> G1, std::vector<std::vector<int>> G2);
 
 private:
+	void editSolutionAfterTwoAlgorithms();
 	bool approximateComputed;
 	bool exactComputed;
 	bool readFromFfile;
+	bool editedSolutionAfterTwoAlgorithms;
+
+	graphComarison_t compareSupergraphs;
+	graphComarison_t compareSubgraphs;
 };
 
